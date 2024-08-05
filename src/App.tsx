6436a312b7c02504f977cab75ccac0346ef50f37
JordsonZ <jordsongalvao@gmail.com>
2024-08-05 15:52:25 -0300

    yes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,77 @@
 import { useState } from "react";
 
-const Square = ({value}) => {
-  const [valor, mudarValor] = useState(value ?? "-");
+const Square = ({value, onSquareClick}) => {
+  let valor = value ?? "";
+  //const [valor, mudarValor] = useState(value ?? "-");
   //const valor = value ?? "-";
 
-  const EscutarClique =() => {
-    console.info("clicado")
-    mudarValor("X");
-  }
+  // const EscutarClique =() => {
+  //   console.info("clicado")
+  //   //mudarValor("X");
+  //   mudarValor(jogador)
+  //   if (jogador === "X"){
+  //     mudarJogador("O")
+  //   } else {
+  //     mudarJogador("X")
+  //   }
+  // };
 
   return (
-  <button className="square" onClick={EscutarClique}> 
+  <button className="square" onClick={onSquareClick}> 
     {valor}
   </button>);
 }
-
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] !== " " && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return false;
+}
 const Board = () => {
+  const [jogador, setJogador] = useState("X");
+  const [squares, setSquares] = useState(Array(9).fill(" "));
+
+  const handleClick = (indice: number) => {
+    if (squares[indice] !== " " || calculateWinner(squares)) return;
+    let copia = squares.slice();
+    copia[indice] = jogador;
+    setSquares(copia);
+    if (jogador === "X"){
+      setJogador("O")
+    } else {
+      setJogador("X")
+    }
+  }
   return (
     <>
-      <h1>Jogo da Ikareai</h1>
+      <h1>Tic-Tac-Toe</h1>
       <div className="board-row"> 
-        <Square value="D" />
-        <Square value="E"/>
-        <Square value="U"/>
+        <Square value={squares[0]} onSquareClick = {() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick = {() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick = {() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square value="V"/>
-        <Square value="E"/>
-        <Square value="L"/>
+        <Square value={squares[3]} onSquareClick = {() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick = {() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick = {() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square value="H"/>
-        <Square value="A"/>
-        <Square value="!"/>
+        <Square value={squares[6]} onSquareClick = {() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick = {() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick = {() => handleClick(8)}/>
       </div>
     </>
   );
